6afa28937f84721b09c406c82baff913521e39da
amarczew <amarczew@11e91674-597b-4138-9f35-7cef2595d825>
2013-01-21 00:27:23 +0000

    git-svn-id: https://svn.code.sf.net/p/btmovies/code/trunk@21 11e91674-597b-4138-9f35-7cef2595d825

--- model/model.py
+++ model/model.py
@@ -372,19 +372,23 @@ def selectAmizadeId(usuario_id, amigo_id):
 	ordem.close()
 	return amizades
 
-############################## RECOMENDAÇÃO
+############################## RECOMENDACAO
 def recomendacaoUsuario(idUsuario):
-	return None
-
+	ordem = conexao.cursor()
+	ordem.execute('SELECT f.*, d.*, wf.*, w.*, af.*, a.*, SQRT(POW(CAST(f.unknown AS INTEGER) - CAST(p.unknown AS INTEGER),2) + POW(CAST(f.action AS INTEGER) - CAST(p.action AS INTEGER),2) + POW(CAST(f.adventure AS INTEGER) - CAST(p.adventure AS INTEGER),2) + POW(CAST(f.animation AS INTEGER) - CAST(p.animation AS INTEGER),2) + POW(CAST(f.children AS INTEGER) - CAST(p.children AS INTEGER),2) + POW(CAST(f.comedy AS INTEGER) - CAST(p.comedy AS INTEGER),2) + POW(CAST(f.crime AS INTEGER) - CAST(p.crime AS INTEGER),2) + POW(CAST(f.documentary AS INTEGER) - CAST(p.documentary AS INTEGER),2) + POW(CAST(f.drama AS INTEGER) - CAST(p.drama AS INTEGER),2) + POW(CAST(f.fantasy AS INTEGER) - CAST(p.fantasy AS INTEGER),2) + POW(CAST(f.filmnoir AS INTEGER) - CAST(p.filmnoir AS INTEGER),2) + POW(CAST(f.horror AS INTEGER) - CAST(p.horror AS INTEGER),2) + POW(CAST(f.musical AS INTEGER) - CAST(p.musical AS INTEGER),2) + POW(CAST(f.mystery AS INTEGER) - CAST(p.mystery AS INTEGER),2) + POW(CAST(f.romance AS INTEGER) - CAST(p.romance AS INTEGER),2) + POW(CAST(f.scifi AS INTEGER) - CAST(p.scifi AS INTEGER),2) + POW(CAST(f.thriller AS INTEGER) - CAST(p.thriller AS INTEGER),2) + POW(CAST(f.war AS INTEGER) - CAST(p.war AS INTEGER),2) + POW(CAST(f.western AS INTEGER) - CAST(p.western AS INTEGER),2)) as distEuc FROM film_perfils p, films f INNER JOIN directors d ON d.iddirector = f.director INNER JOIN writersfilm wf ON wf.idfilm = f.filmid INNER JOIN writers w ON w.idwriter = wf.idwriter INNER JOIN actorsfilm af ON af.idfilm = f.filmid INNER JOIN actors a ON a.idactor = af.idactor WHERE p.idUsuario = %s ORDER BY distEuc, title ASC', [idUsuario])
+	auxFilms = ordem.fetchall()
+	films = mountFilms(auxFilms)
+	ordem.close()
+	return films
 
 ############################## Cadastro Film Test
 #actors = ['actor1', 'actor2', 'actor3']
 #writers = ['writer1', 'writer2', 'writer3']
 #insertFilm('film1', 'film1.com', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', 9.0, 'director1', 2012, 'sinopse1', 'image_1.jpg', actors, writers)
 
-filmes = selectUsuarioLogin("teste", "senha")
+filmes = recomendacaoUsuario(1)
 for filme in filmes:
-	print filme.name
+	print filme.title
 
 conexao.close()
 
